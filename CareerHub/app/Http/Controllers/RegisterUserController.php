715c60e36096a1e8cf613e56a677a3e29d7d79f0
hendrahartanto <hendrazhuo10@gmail.com>
2024-11-26 21:31:03 +0700

    feat: improved login form

--- CareerHub/app/Http/Controllers/RegisterUserController.php
+++ CareerHub/app/Http/Controllers/RegisterUserController.php
@@ -14,89 +14,81 @@ use App\Models\User;
 
 class RegisterUserController extends Controller
 {
-    public function register(Request $request)
-    {
-//        dd($request);
-        $user_validator = Validator::make($request->all(), [
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|confirmed|min:8',
-            'role' => 'required|string',
-        ]);
+  public function register(Request $request)
+  {
+    //        dd($request);
+    $user_validator = Validator::make($request->all(), [
+      'first_name' => 'required|string|max:255',
+      'last_name' => 'required|string|max:255',
+      'email' => 'required|email|unique:users,email',
+      'password' => 'required|confirmed|min:8',
+      'role' => 'required|string',
+    ]);
 
-        if ($user_validator->fails()) {
-            return redirect()->back()
-                ->withErrors($user_validator)
-                ->withInput();
-        }
-
-        if($request->input('role') == "Employee") {
-            $user = User::create([
-                'id' => Str::uuid(),
-                'first_name' => $request->input('first_name'),
-                'last_name' => $request->input('last_name'),
-                'email' => $request->input('email'),
-                'password' => bcrypt($request->input('password')),
-                'role' => $request->input('role'),
-            ]);
-            Auth::login($user);
-        }
-        else {
-            $company_validator = Validator::make($request->all(), [
-                'company_name' => 'required',
-                'country' => 'required',
-                'location' => 'required',
-                'city' => 'required',
-                'position_name' => 'required',
-                'job_level' => 'required',
-                'job_summary' => 'required',
-            ]);
-
-            if ($company_validator->fails()) {
-                return redirect()->back()
-                    ->withErrors($company_validator)
-                    ->withInput();
-            }
-
-            $company = Company::create([
-                'company_name' => $request->input('company_name'),
-                'country' => $request->input('country'),
-                'location' => $request->input('location'),
-                'city' => $request->input('city'),
-                'position_name' => $request->input('position_name'),
-                'job_level' => $request->input('job_level'),
-                'job_summary' => $request->input('job_summary'),
-            ]);
-        }
-
-        echo "User registered successfully!";
-
-        return redirect('/login');
-
+    if ($user_validator->fails()) {
+      return redirect()->back()
+        ->withErrors($user_validator)
+        ->withInput();
     }
 
-    /**
-     * @throws ConnectionException
-     */
-    public function test(){
-        $response = Http::accept('application/json')->get('http://127.0.0.1:5000/csv_data');
-        $data = json_decode($response->body(), true);
-        dd($response->json());
-//        if ($response->successful()) {
-//            // Get the raw body of the response and decode it manually
-//            $data = json_decode($response->body(), true);
-//
-//            // Check if the data was decoded successfully
-//            if ($data !== null) {
-//                dd($data); // Display the data
-//            } else {
-//                dd("ARGHH", $response->body());
-//            }
-//        } else {
-//            // Handle the error
-//            dd("Error fetching data from Flask API", $response->status(), $response->body());
-//        }
+    if ($request->input('role') == "Employee") {
+      $user = User::create([
+        'id' => Str::uuid(),
+        'first_name' => $request->input('first_name'),
+        'last_name' => $request->input('last_name'),
+        'email' => $request->input('email'),
+        'password' => bcrypt($request->input('password')),
+        'role' => $request->input('role'),
+      ]);
+      Auth::login($user);
+    } else {
+      $company_validator = Validator::make($request->all(), [
+        'company_name' => 'required',
+        'country' => 'required',
+        'location' => 'required',
+        'city' => 'required',
+      ]);
+
+      if ($company_validator->fails()) {
+        return redirect()->back()
+          ->withErrors($company_validator)
+          ->withInput();
+      }
+
+      $company = Company::create([
+        'company_name' => $request->input('company_name'),
+        'country' => $request->input('country'),
+        'location' => $request->input('location'),
+        'city' => $request->input('city'),
+      ]);
     }
+
+    echo "User registered successfully!";
+
+    return redirect('/login');
+  }
+
+  /**
+   * @throws ConnectionException
+   */
+  public function test()
+  {
+    $response = Http::accept('application/json')->get('http://127.0.0.1:5000/csv_data');
+    $data = json_decode($response->body(), true);
+    dd($response->json());
+    //        if ($response->successful()) {
+    //            // Get the raw body of the response and decode it manually
+    //            $data = json_decode($response->body(), true);
+    //
+    //            // Check if the data was decoded successfully
+    //            if ($data !== null) {
+    //                dd($data); // Display the data
+    //            } else {
+    //                dd("ARGHH", $response->body());
+    //            }
+    //        } else {
+    //            // Handle the error
+    //            dd("Error fetching data from Flask API", $response->status(), $response->body());
+    //        }
+  }
 }
-
--- CareerHub/public/js/registerFormRender.js
+++ CareerHub/public/js/registerFormRender.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const roleInput = document.getElementById('role')
     const companyDetailForm = document.getElementById('company-detail-form')
+    const employeeDetailForm = document.getElementById('employee-detail-form')
 
     function toggleRoleInput() {
         if (roleInput.value === 'Company') {
             companyDetailForm.style.display = 'flex';
+            employeeDetailForm.style.display = 'none';
         } else {
             companyDetailForm.style.display = 'none';
+            employeeDetailForm.style.display = 'flex';
         }
     }
     toggleRoleInput();
