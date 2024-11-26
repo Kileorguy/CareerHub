76ed6a73006d4b626be36447d6e952fbfc61eb6c
Hendi K <hendikrisnanto@rocketmail.com>
2024-11-26 21:03:35 +0700

    Feat: .dockerIgnore and DockerCompose using .env

--- CareerHub/vite.config.js
+++ CareerHub/vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     preview: {
-        port: 3000,
+        port: 4173,
     },
     build: {
         outDir: 'public/build'
