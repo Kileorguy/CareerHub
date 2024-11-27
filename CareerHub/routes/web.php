7c72a059449c83590b47753dc93b50c902128c09
hendrahartanto <hendrazhuo10@gmail.com>
2024-11-27 12:18:56 +0700

    feat: custom role middle ware & refactor routes structure

--- CareerHub/routes/web.php
+++ CareerHub/routes/web.php
@@ -13,30 +13,33 @@ use App\Http\Middleware\isLoggedIn;
 use App\Http\Middleware\isLoggedOut;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(isLoggedIn::class)->group(function () {
-  Route::get('/', [CompanyController::class, 'getAllCompanies']);
-  Route::get('/profile', [LoginUserController::class, 'profile'])->name('profile');
-  //    Route::view('profile', 'profile');
-});
-
-Route::middleware(isLoggedOut::class)->group(function () {
+//Guest routes (not authenticated or signed in)
+Route::middleware('guest')->group(function () {
   Route::view('/login', 'auth.login');
   Route::view('/register', 'auth.register');
+  Route::post('/register', [RegisterUserController::class, 'register'])->name('register');
+  Route::post('/login', [LoginUserController::class, 'login'])->name('login');
 });
 
+//Authenticated user routes (looged in ignoring roles)
+Route::middleware('auth')->group(function () {
+  Route::get('/', [CompanyController::class, 'getAllCompanies']);
+  Route::get('/profile', [LoginUserController::class, 'profile'])->name('profile');
+  Route::post('/updateProfile', [LoginUserController::class, 'updateProfile'])->name('updateProfile');
+  Route::post('/changePassword', [LoginUserController::class, 'changePassword'])->name('changePassword');
+  Route::get('/logout', [LoginUserController::class, 'logout'])->name('logout');
+});
 
+//Authenticated user with employee role routes
+Route::middleware(['auth', 'role:employee'])->group(function () {
+  Route::post('/insertExperience', [ExperienceController::class, 'InsertExperience'])->name('insertExperience');
+  Route::post('/updateExperience/{id}', [ExperienceController::class, 'updateExperience'])->name('updateExperience');
+  Route::post('/insertEducation', [UserEducationController::class, 'InsertEducation'])->name('insertEducation');
+  Route::post('/updateEducation/{id}', [UserEducationController::class, 'updateEducation'])->name('updateEducation');
+  Route::post('/insertCertificate', [UserCertificateController::class, 'InsertCertificate'])->name('insertCertificate');
+  Route::post('/updateCertificate/{id}', [UserCertificateController::class, 'updateCertificate'])->name('updateCertificate');
+});
 
-Route::post('/register', [RegisterUserController::class, 'register'])->name('register');
-Route::post('/insertExperience', [ExperienceController::class, 'InsertExperience'])->name('insertExperience');
-Route::post('/updateExperience/{id}', [ExperienceController::class, 'updateExperience'])->name('updateExperience');
-Route::post('/insertEducation', [UserEducationController::class, 'InsertEducation'])->name('insertEducation');
-Route::post('/updateEducation/{id}', [UserEducationController::class, 'updateEducation'])->name('updateEducation');
-Route::post('/insertCertificate', [UserCertificateController::class, 'InsertCertificate'])->name('insertCertificate');
-Route::post('/updateCertificate/{id}', [UserCertificateController::class, 'updateCertificate'])->name('updateCertificate');
-Route::post('/updateProfile', [LoginUserController::class, 'updateProfile'])->name('updateProfile');
-Route::post('/changePassword', [LoginUserController::class, 'changePassword'])->name('changePassword');
-Route::post('/login', [LoginUserController::class, 'login'])->name('login');
-Route::get('/logout', [LoginUserController::class, 'logout'])->name('logout');
 
 // Route::view('/search/{query}', 'search');
 // Route::view('/profile', 'profile');
